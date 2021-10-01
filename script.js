4e36b280205a5d1670e60ed0d9e5330ba759f48a
skeyan <eyan0749@gmail.com>
2021-10-01 13:02:01 -0400

    Implement add row functionality

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
 
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
+    console.log("Clicked Add Row");
+
+    /* Add new row element to table */
+    let table = document.getElementById("grid");
+    let newRow = document.createElement("tr");
+
+    if (numRows == 0) { // Edge case
+        let newCol = document.createElement("td");
+        newRow.appendChild(newCol);
+        table.appendChild(newRow);
+
+        /* Increment count */
+        numRows += 1;
+        numCols += 1;
+    } else {
+        for (let i = 0; i < numCols; i++) {
+            let newCol = document.createElement("td");
+            newRow.appendChild(newCol);
+        }
+        table.appendChild(newRow);
+
+        /* Increment count */
+        numRows += 1;
+    }
 }
 //Add a column
 function addC() {
